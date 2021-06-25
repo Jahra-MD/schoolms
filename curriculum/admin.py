3b8ec2f3106cf30d39de6b9ad0546d271fc78c48
Jahra <mohammedjahra@gmail.com>
2021-06-25 11:07:33 +0530

    Initial commit

---
+++ curriculum/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from curriculum.models import Standard, Subject, Lesson ,Comment,Reply
+
+admin.site.register(Standard)
+admin.site.register(Subject)
+admin.site.register(Lesson)
+admin.site.register(Comment)
+admin.site.register(Reply)
